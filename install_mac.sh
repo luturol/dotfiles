fc0af806b69693f770a3fb92bef6dd5856b68c6a
Rafael Ahrons <rafael.ahrons@gmail.com>
2024-10-25 11:03:08 -0300

    Create install_mac.sh

---
+++ install_mac.sh
@@ -0,0 +1,17 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install --cask dotnet-sdk
+
+brew install --cask another-redis-desktop-manager
+
+brew install docker
+
+brew install docker-compose
+
+brew install colima
+
+brew install --cask visual-studio-code
+
+brew install git
+
+brew install --cask google-chrome
